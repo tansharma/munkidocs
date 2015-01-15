8e58a52d6818a0b6745db3997f567e9e39966587
tansharma <tansharma@gmail.com>
2015-01-15 12:30:53 +0000

    import icons for packages added to MunkiAdmin
    Useful if you're adding packages to MunkiAdmin directly or via AutoPkgr. Not required if you use command line to import packages.

---
+++ Setup/Scripts/iconimporter.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+# Script to setup icon importer function for Munki Admin
+curl -O https://munki.googlecode.com/git-history/Munki2/code/client/iconimporter
+
+# Move and rename iconimporter
+mv iconimporter /usr/local/munki/iconimporter.py
+
+# modify permissions
+chmod +x /usr/local/munki/iconimporter.py
+
+# Navigate to munki directory and run icon importer
+cd /usr/local/munki
+./iconimporter.py /Volumes/LocalDataHD/Munki/munki_repo
+
+exit 0
