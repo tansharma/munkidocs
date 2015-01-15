5a20d2f98c41e3cde0d47568f32d3d411a5fe912
tansharma <tansharma@gmail.com>
2015-01-15 12:34:40 +0000

    setup user for munkiadmin
    setup a user called munkiadmin and add it to group 'munki'
    change permissions for group 'munki' so that it can write to your munkiwebadmin virtual environment

---
+++ Setup/Scripts/usersettings.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+# Edit account attributes for munkiwebadmin
+dscl . create /Users/munkiwebadmin home /var/empty
+dscl . create /Users/munkiwebadmin passwd *
+
+# Create a group and add munkiwebadmin to it
+dseditgroup -o create -n . munki
+dseditgroup -o edit -a munkiwebadmin -t user munki
+
+# Give permissions to 'munki' group to read and write in munkiwebadmin_env
+chgrp -R munki /Users/Shared/munkiwebadmin_env
+chmod -R g+rw /Users/Shared/munkiwebadmin_env
+
+# Assign group read/write access to your munki repo
+chgrp -R munki /Volumes/LocalDataHD/Munki/munki_repo
+chmod -R g+rw  /Volumes/LocalDataHD/Munki/munki_repo
+
+exit 0
