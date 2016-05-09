ba51a5b7f6320291bcc891fefbd389846e68341f
ts50 <t.sharma@sussex.ac.uk>
2016-05-09 16:54:10 +0100

    rename Volume

--- Setup/OSX/osx-scripts/munki-folder-setup.sh
+++ Setup/OSX/osx-scripts/munki-folder-setup.sh
@@ -2,10 +2,10 @@
 
 # This script will create the required folders for Munki and then create a symlink between
 # your munki_repo directory and the default directory used by your web server. Our puppet
-# managed workstations create a Volume called LocalDataHD which stores the local user profile. 
+# managed workstations create a Volume called LocalDataHD which stores the local user profile.
 # This Volume may be different on other deployments, and should be edited as necessary.
 
-# Navigate to LocalDataHD on your server machine
+# Replace LocalDataHD with Volume where you would like your _munki_repo_ setup
 cd /Volumes/LocalDataHD
 
 # Create a directory called Munki, with a sub directory called munki_repo. Within munki_repo
@@ -18,6 +18,6 @@ mkdir Munki/munki_repo/pkgs
 mkdir Munki/munki_repo/pkgsinfo
 
 # Modify permissions for directory munki_repo
-chmod -R a+rX /Volumes/LocalDataHD/Munki/munki_repo
+chmod -R a+rX /Volumes/<your_volume>/Munki/munki_repo
 
 exit 0
